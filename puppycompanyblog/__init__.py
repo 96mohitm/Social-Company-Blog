1629638290cd2d165cd50fc91465719a47843700
Mohit Musaddi <96mohitm@gmail.com>
2019-07-26 22:20:08 +0530

    Model for user and BlogPost created.

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,6 +1,33 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+
+################
+#### DATABASE SETUP ####
+#####################
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Migrate(app, db)
+
+###################################
+# login configs
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'users.login'
+
+
+
+
+###################################
 
 
 from puppycompanyblog.core.views import core
