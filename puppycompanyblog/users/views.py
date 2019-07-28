d8ba65e0b228b11b53ad8269e96b760bb160e220
Mohit Musaddi <96mohitm@gmail.com>
2019-07-28 08:49:55 +0530

    Site running and minor bug fixed.

--- puppycompanyblog/users/views.py
+++ puppycompanyblog/users/views.py
@@ -59,36 +59,53 @@ def logout():
 
 
 # account (update UserForm)
-@users.route('/account',methods=['GET','POST'])
+@users.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
 
     form = UpdateUserForm()
+
     if form.validate_on_submit():
 
         if form.picture.data:
             username = current_user.username
-            pic = add_profile_pic(form.picture.data,username)
+            pic = add_profile_pic(form.picture.data, username)
             current_user.profile_image = pic
 
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
-        flash('User Account Updated!')
+        flash('User Account Updated')
         return redirect(url_for('users.account'))
 
-    elif request.method == "GET":
+    elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
 
-    profile_image = url_for('static',filename='profile_pics/'+current_user.profile_image)
-    return render_template('account.html',profile_image=profile_image,form=form)
+    profile_image = url_for('static', filename='profile_pics/' + current_user.profile_image)
+    return render_template('account.html', profile_image=profile_image, form=form)
 
-
-# user's list of Blog posts
 @users.route("/<username>")
 def user_posts(username):
     page = request.args.get('page',1,type=int)
     user = User.query.filter_by(username=username).first_or_404()
     blog_posts = BlogPost.query.filter_by(author=user).order_by(BlogPost.date.desc()).paginate(page=page,per_page=5)
     return render_template('user_blog_posts.html',blog_posts=blog_posts,user=user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# user's list of Blog posts
